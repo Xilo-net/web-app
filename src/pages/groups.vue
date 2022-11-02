dcb9b33ef07feef2af9072bdfff565a0f68b63c3
Luis Angel Guzmán Iribe <A01741757@itesm.mx>
2022-11-02 17:17:52 -0600

    feat: api service file

--- src/pages/groups.vue
+++ src/pages/groups.vue
@@ -7,7 +7,7 @@
 </route>
 
 <script setup>
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import { Menu, MenuButton, MenuItem, MenuItems, Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
 useHead({ title: 'About' });
 
 const groupOptions = ref(
@@ -24,7 +24,7 @@ const usersRanking = ref(
   Array(10).fill({ name: 'Nombre Apellido Apellido', points: 59 }),
 );
 
-
+const open = ref(false)
 </script>
  
 <template>
@@ -37,8 +37,8 @@ const usersRanking = ref(
               Estadisticas del grupo
             </h1>
             <Menu as="div" class="inline-block text-left absolute right-0 top-0">
-              <MenuButton
-                class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-lime-400 focus:ring-offset-2 focus:ring-offset-gray-100">
+              <MenuButton class="intButton">
+                <span class="material-icons">expand_more</span>
                 Seleccionar grupo
               </MenuButton>
               <transition enter-active-class="transition ease-out duration-100"
@@ -110,8 +110,7 @@ const usersRanking = ref(
               Miembros del grupo
             </h1>
             <div class="inline-block text-left absolute right-0 top-0">
-              <button
-                class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-lime-400 focus:ring-offset-2 focus:ring-offset-gray-100">
+              <button class="intButton" @click="open = true">
                 Agregar miembros
               </button>
             </div>
@@ -143,9 +142,13 @@ const usersRanking = ref(
         </div>
 
         <div class="bg-white drop-shadow-md rounded-md mt-3 p-6">
-          <h1 class=" font-bold tracking-tight text-gray-900 sm:text-3xl">
-            Usuario seleccionado
-          </h1>
+          <div class="relative">
+            <h1 class="font-bold tracking-tight text-gray-900 sm:text-3xl">
+              Nombre usuario seleccionado
+            </h1>
+            <img class="w-20 h-20 top-0 right-0 absolute rounded-full"
+              src="../../src/assets/img/default-profile-pic-yoda.jpg" alt="Rounded avatar">
+          </div>
           <h2 class="font-semibold sm:text-xl mt-3 tracking-tight">
             Puntaje Acumulado por día: {{ 59 }}
           </h2>
@@ -165,5 +168,53 @@ const usersRanking = ref(
         </div>
       </div>
     </div>
+
+    <TransitionRoot as="template" :show="open">
+      <Dialog as="div" class="relative z-10" @close="open = false">
+        <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100"
+          leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+          <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </TransitionChild>
+
+        <div class="fixed inset-0 z-10 overflow-y-auto">
+          <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <TransitionChild as="template" enter="ease-out duration-300"
+              enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200"
+              leave-from="opacity-100 translate-y-0 sm:scale-100"
+              leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+              <DialogPanel
+                class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div class="sm:flex sm:items-start">
+                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <DialogTitle as="h3" class="text-lg font-medium leading-6 text-gray-900">Agregar miembros
+                      </DialogTitle>
+                      <div class="mt-2">
+                        <p class="text-sm text-gray-500">Aquí va a ir una interfaz para agregar nuevos usuarios</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button type="button"
+                    class="inline-flex w-full justify-center rounded-md border border-transparent bg-lime-500 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-lime-600 focus:outline-none focus:ring-2 focus:ring-lime-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                    @click="open = false">Agregar</button>
+                  <button type="button"
+                    class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    @click="open = false" ref="cancelButtonRef">Cancelar</button>
+                </div>
+              </DialogPanel>
+            </TransitionChild>
+          </div>
+        </div>
+      </Dialog>
+    </TransitionRoot>
   </div>
 </template>
+
+<style>
+.intButton {
+  @apply inline-flex w-full justify-center rounded-md border border-gray-300 bg-white pr-4 pl-2 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-lime-400 focus:ring-offset-2 focus:ring-offset-gray-100;
+}
+</style>
